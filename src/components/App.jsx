80b6255d7931e2abf0b6022324236223d5634382
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-08-03 00:10:16 +0300

    finnal_save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,77 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './Button/Button';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 import { Searchbar } from './Searchbar/Searchbar';
 import { getImg } from './services/getImg';
-export class App extends Component {
-  state = {
-    searchImg: '',
-    images: [],
-    isLoading: false,
-    error: null,
-    page: 1,
-    onSelectImage: null,
-    modalOpen: false,
-    totalImages:0,
+
+export const App = () => {
+  const [searchImg, setSearchImg] = useState('');
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [onSelectImage, setOnSelectImage] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [totalImages, setTotalImages] = useState(0);
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      if(!searchImg) return
+      try {
+        setIsLoading(true);
+        const data = await getImg(searchImg, page);
+        setImages(prevImages=>[...prevImages, ...data.hits]);
+        setTotalImages(data.totalHits);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchPostData();
+  }, [searchImg, page]);
+
+  const hendleSearch = query => {
+    if(searchImg!==query){
+      setSearchImg(query)
+      setPage(1)
+      setImages([])
+      setTotalImages(0)
+    }
   };
 
+  const onMoreBtnClick = () => {
+    setPage(prevPage=>prevPage+1)
+  };
+
+  const openModal = onSelectImage => {
+    setModalOpen(true)
+    setOnSelectImage(onSelectImage)
+  };
+  const closeModal = () => {
+    setModalOpen(false)
+    setOnSelectImage(null)
+  };
+  const showBtn = images.length + 1 < totalImages; 
+    return (
+      <>
+        <Searchbar hendleSearch={hendleSearch} />
+        <ImageGallery data={images} onItemClick={openModal} />
+        {images.length > 0 && showBtn && <Button onMoreBtnClick={onMoreBtnClick} />}
+        {isLoading && <Loader />}
+        {modalOpen && (
+          <Modal
+            image={onSelectImage}
+            onClose={closeModal}
+          />
+        )}
+      </>
+    );
+  }
+
+
+/* export class App extends Component {
   hendleSearch = searchImg => {
     this.setState({ searchImg, page: 1, images: [], totalImages:0 });
   };
@@ -31,29 +86,6 @@ export class App extends Component {
   closeModal = () => {
     this.setState({ modalOpen: false, onSelectImage: null });
   };
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchImg, page, images } = this.state;
-    if (
-      prevState.searchImg !== this.state.searchImg ||
-      prevState.page !== page
-    ) {
-      try {
-        if (images.length === 0 || page === 1) {
-          this.setState({ isLoading: true });
-        }
-        const data = await getImg(searchImg, page);
-        this.setState({
-          images: [...images, ...data.hits],
-          totalImages: data.totalHits,
-          isLoading: false,
-        });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
   render() {
     const { isLoading, images, onSelectImage, modalOpen, totalImages } = this.state;
     const showBtn = images.length + 1 < totalImages; 
@@ -73,4 +105,27 @@ export class App extends Component {
       </>
     );
   }
-}
+} */
+// async componentDidUpdate(prevProps, prevState) {
+//   const { searchImg, page, images } = this.state;
+//   if (
+//     prevState.searchImg !== this.state.searchImg ||
+//     prevState.page !== page
+//   ) {
+//     try {
+//       if (images.length === 0 || page === 1) {
+//         this.setState({ isLoading: true });
+//       }
+//       const data = await getImg(searchImg, page);
+//       this.setState({
+//         images: [...images, ...data.hits],
+//         totalImages: data.totalHits,
+//         isLoading: false,
+//       });
+//     } catch (error) {
+//       this.setState({ error });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,39 @@
-import { Component } from "react";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import css from '../Searchbar/Searchbar.module.css'
-export class Searchbar extends Component {
+
+export const Searchbar = ({hendleSearch}) =>{
+    const [value, setValue] = useState('')
+    const handleChange=(event)=>{
+        setValue(event.target.value)
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        hendleSearch(value)
+    }
+    return (
+        <header className={css.searchbar}>
+            <form className="form" onSubmit={handleSubmit}>
+            <button type="submit" className={css.button}>
+                <span className="button-label">Search</span>
+            </button>
+            <input
+                className={css.input}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                onChange={handleChange}
+                value={value}
+            />
+            </form>
+        </header>
+    );
+}
+Searchbar.propTypes = {
+    hendleSearch: PropTypes.func.isRequired,
+}; 
+/* export class Searchbar extends Component {
     state = {
         value: ''
     }
@@ -36,6 +68,4 @@ export class Searchbar extends Component {
         );
     }
 };
-Searchbar.propTypes = {
-    hendleSearch: PropTypes.func.isRequired,
-};
+*/
