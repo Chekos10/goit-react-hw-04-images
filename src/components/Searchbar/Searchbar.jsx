876068dc752d2a5baf925e5bfec75c9d1b8823d6
Chekos10 <119345370+Chekos10@users.noreply.github.com>
2023-08-03 00:20:38 +0300

    remove_comments

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -35,39 +35,3 @@ export const Searchbar = ({hendleSearch}) =>{
 Searchbar.propTypes = {
     hendleSearch: PropTypes.func.isRequired,
 }; 
-/* export class Searchbar extends Component {
-    state = {
-        value: ''
-    }
-
-    handleChange=({target:{value}})=>{
-        this.setState({value})
-    }
-
-    handleSubmit=(event)=>{
-        event.preventDefault()
-        this.props.hendleSearch(this.state.value)
-    }
-
-    render(){
-        return (
-            <header className={css.searchbar}>
-                <form className="form" onSubmit={this.handleSubmit}>
-                <button type="submit" className={css.button}>
-                    <span className="button-label">Search</span>
-                </button>
-                <input
-                    className={css.input}
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    onChange={this.handleChange}
-                    value={this.state.value}
-                />
-                </form>
-            </header>
-        );
-    }
-};
-*/
